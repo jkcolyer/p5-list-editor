0dc8eca1c797bb4bf41d0660feac2c8ddd4b358c
jkcolyer <jkcolyer@umich.edu>
2026-04-06 00:26:40 -0400

    more tests

--- List_tests.cpp
+++ List_tests.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 // Add your test cases here
 
-TEST(test_empty_and_size) {
+TEST(test_list_empty_and_size) {
     List<int> list;
     ASSERT_TRUE(list.empty());
     ASSERT_EQUAL(list.size(), 0);
@@ -14,7 +14,7 @@ TEST(test_empty_and_size) {
     ASSERT_EQUAL(list.size(), 1);
 }
 
-TEST(test_push_front_and_back) {
+TEST(test_list_push_front_and_back) {
     List<int> list;
     list.push_back(2);
     list.push_front(1);
@@ -24,7 +24,7 @@ TEST(test_push_front_and_back) {
     ASSERT_EQUAL(list.size(), 3);
 }
 
-TEST(test_pop_front_and_back) {
+TEST(test_list_pop_front_and_back) {
     List<int> list;
     list.push_back(1);
     list.push_back(2);
@@ -36,7 +36,7 @@ TEST(test_pop_front_and_back) {
     ASSERT_EQUAL(list.size(), 1);
 }
 
-TEST(test_clear) {
+TEST(test_list_clear) {
     List<int> list;
     list.push_back(1);
     list.push_back(2);
@@ -44,7 +44,7 @@ TEST(test_clear) {
     ASSERT_TRUE(list.empty());
 }
 
-TEST(test_copy_constructor) {
+TEST(test_iterator_copy_constructor) {
     List<int> a;
     a.push_back(1);
     a.push_back(2);
@@ -54,7 +54,7 @@ TEST(test_copy_constructor) {
     ASSERT_EQUAL(b.front(), 1);
 }
 
-TEST(test_assignment_operator) {
+TEST(test_iterator_assignment_operator) {
     List<int> a;
     a.push_back(1);
     a.push_back(2);
@@ -90,7 +90,7 @@ TEST(test_iterator_decrease) {
     ASSERT_EQUAL(*it, 2);
 }
 
-TEST(test_insert) {
+TEST(test_iterator_insert) {
     List<int> list;
     list.push_back(1);
     list.push_back(3);
@@ -106,7 +106,7 @@ TEST(test_insert) {
     ASSERT_EQUAL(*it, 3);
 }
 
-TEST(test_erase) {
+TEST(test_iterator_erase) {
     List<int> list;
     list.push_back(1);
     list.push_back(2);
@@ -117,6 +117,52 @@ TEST(test_erase) {
     ASSERT_EQUAL(list.size(), 2);
     ASSERT_EQUAL(list.front(), 1);
     ASSERT_EQUAL(list.back(), 3);
+}
+
+TEST(test_list_copy_2) {
+    List<int> a;
+    a.push_back(1);
+    a.push_back(2);
+    List<int> b;
+    b = a;
+    a.push_back(3);
+    a.pop_front();
+    ASSERT_EQUAL(b.size(), 2);
+    ASSERT_EQUAL(b.front(), 1);
+    ASSERT_EQUAL(b.back(), 2);
+}
+
+TEST(test_list_copy_3) {
+    List<int> a;
+    a.push_back(1);
+    List<int> b;
+    b.push_back(10);
+    b.push_back(11);
+    b.push_back(12);
+    b = a;
+    ASSERT_EQUAL(b.size(), 1);
+    ASSERT_EQUAL(b.front(), 1);
+    ASSERT_EQUAL(b.back(), 1);
+}
+
+TEST(test_list_copy_itself) {
+    List<int> a;
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    a = a;
+    ASSERT_EQUAL(a.size(), 3);
+    ASSERT_EQUAL(a.front(), 1);
+    ASSERT_EQUAL(a.back(), 3);
+}
+
+TEST(test_list_copy_empty) {
+    List<int> a;
+    List<int> b;
+    b.push_back(1);
+    b.push_back(2);
+    b = a;
+    ASSERT_TRUE(b.empty());
 }
 
 TEST_MAIN()
